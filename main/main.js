ad5379214806b92194acddf5a78a5fb00f2759d7
hutingting119 <1912257350@qq.com>
2016-05-06 23:51:07 +0800

    LCD main3

--- main/main.js
+++ main/main.js
@@ -2,33 +2,40 @@ function printReceipt(inputs){
     var loadAllLcd = LCD_Digits();
     var numbers=splitNumbers(inputs);
     var lcdDigits=matchLcdDigits(loadAllLcd,numbers);
-    toRecept(lcdDigits);
+    toPrint(lcdDigits);
 }
 
 function splitNumbers(inputs) {
     inputs+='';
     var numbers=inputs.split('');
+
     for(var i=0;i<numbers.length;i++){
         numbers[i]=parseInt(numbers[i]);
     }
+
     return numbers;
 }
 
 function matchLcdDigits(loadAllLcd,numbers) {
     var lcdDigits=[];
+
         for(var i=0;i<numbers.length;i++){
             lcdDigits.push(loadAllLcd[numbers[i]]);
         }
+
     return lcdDigits;
 }
 
-function toRecept(lcdDigits) {
+function toPrint(lcdDigits) {
     var output='';
+
     for(var i=0;i<3;i++){
+
         for(var j=0;j<lcdDigits.length;j++) {
             output += lcdDigits[j][i] + ' ';
         }
         output+='\n';
     }
+
     console.log(output);
 }
